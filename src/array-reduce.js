d5f390136b17a4b03de3548509f7875db82d9a89
Gabriel Simek <gabrielsimek@outlook.com>
2021-05-11 17:55:41 -0700

    all tests required tests passing

--- src/array-reduce.js
+++ src/array-reduce.js
@@ -21,3 +21,32 @@ export const addPurchases = items => {
   
 };
 
+export const countNumberOfElements = array => {
+ 
+  return array.reduce((accumulator) => {
+    accumulator++;
+    return accumulator
+    ;
+    
+  }, 0);
+  
+  
+};
+export const returnNames = array => {
+  return array.reduce((accumulator, item) => {
+    accumulator.push(item.name);
+    return accumulator;
+        
+  }, []);
+
+};
+
+export const reversedString = string => {
+  return string.split('').reduce((accumulator, item) => {
+    console.log(item + accumulator);
+    return item + accumulator;    
+  }, '');
+  
+
+};
+
